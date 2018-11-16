9e5b64b6a1071dd63d59fddf3ada1a1110620c2d
Younès Rabii <rabiiyounes@outlook.com>
2018-11-16 14:55:07 +0100

    Correction des ERREURS de Hugo

--- Sentiant/Model/Ant.py
+++ Sentiant/Model/Ant.py
@@ -4,26 +4,19 @@ class Ant(SolidEntity):
 
     HP = 2
 
-    def __init__(self,id,name,team):
+    def __init__(self, id, name, team):
         super().__init__(self,id)
         self.name = name
         self.team = team
-        self.holding=None
+        self.holding = None
 
     def pickup(self,entity):
-    "Store the picked up entity in Ant.holding"
+        """Store the picked up entity in Ant.holding"""
         if (self.holding is None):
-            self.holding=entity
+            self.holding = entity
 
     def drop(self):
-    "Empty the Ant.holding if it isn't None "
+        """Empty the Ant.holding if it isn't None """
         if (self.holding is None)== False:
-            self.holding=None
-
-
-
-
-
-
-        pass
+            self.holding = None
 
