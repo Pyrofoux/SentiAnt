41879d4d7850e6ecd64ce16802b9c7d41f67c382
Hugo <hfournier001@ensc.fr>
2018-11-16 14:42:12 +0100

    Ant !

--- Sentiant/Model/Ant.py
+++ Sentiant/Model/Ant.py
@@ -1,7 +1,27 @@
-class Ant:
+from Sentiant.Model import SolidEntity
+
+class Ant(SolidEntity):
 
     HP = 2
 
-    def __init__(self):
+    def __init__(self,id,name,team):
+        super().__init__(self,id)
+        self.name = name
+        self.team = team
+        self.holding=None
+
+    def pickup(entity):
+        if (self.holding is None):
+            self.holding=entity
+
+    def drop(self.holding):
+        if (self.holding is None)== False:
+            self.holding=None
+
+
+
+
+
+
         pass
 
