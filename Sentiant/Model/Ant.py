30c47cd586d4237b43a33993b22d990a07980cd1
Hugo <hfournier001@ensc.fr>
2018-11-16 14:44:03 +0100

    Ant !

--- Sentiant/Model/Ant.py
+++ Sentiant/Model/Ant.py
@@ -10,11 +10,13 @@ class Ant(SolidEntity):
         self.team = team
         self.holding=None
 
-    def pickup(entity):
+    def pickup(self,entity):
+    "Store the picked up entity in Ant.holding"
         if (self.holding is None):
             self.holding=entity
 
-    def drop(self.holding):
+    def drop(self):
+    "Empty the Ant.holding if it isn't None "
         if (self.holding is None)== False:
             self.holding=None
 
