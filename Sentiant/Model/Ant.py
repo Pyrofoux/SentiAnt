dde2a30411445b4ceb5c7b308d1debc1f4d6daf3
Hugo <hfournier001@ensc.fr>
2018-11-20 17:30:42 +0100

    notes Ant

--- Sentiant/Model/Ant.py
+++ Sentiant/Model/Ant.py
@@ -3,13 +3,14 @@ from Sentiant.Model import SolidEntity
 
 class Ant(SolidEntity):
 
-    HP = 2
+    HP = 2 //à voir
 
     def __init__(self, id, name, team):
         super().__init__(self,id)
         self.name = name
         self.team = team
         self.holding = None
+        //radius de vision? FoV
 
     def pickup(self,entity):
         """Store the picked up entity in Ant.holding"""
@@ -20,4 +21,6 @@ class Ant(SolidEntity):
         """Empty the Ant.holding if it isn't None """
         if (self.holding is None)== False:
             self.holding = None
+
+
 
