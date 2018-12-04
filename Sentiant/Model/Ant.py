706310ef346ae204dc5eacc40bc8d42348e921e1
Hugo <hfournier001@ensc.fr>
2018-12-04 17:10:15 +0100

    commentaires + attributs Ant

--- Sentiant/Model/Ant.py
+++ Sentiant/Model/Ant.py
@@ -3,14 +3,13 @@ from Sentiant.Model import SolidEntity
 
 class Ant(SolidEntity):
 
-    HP = 2 //à voir
-
     def __init__(self, id, name, team):
         super().__init__(self,id)
-        self.name = name
+        self.name = name #id of the ant, string of 5 chars
         self.team = team
-        self.holding = None
-        //radius de vision? FoV
+        self.holding = None #by default, the ant doesn't carry anything
+        self.HP = 2 #number of hits the ant can take before dying
+        self.FoV=7  #Field Of View : number of cells the ant can view, center being itself
 
     def pickup(self,entity):
         """Store the picked up entity in Ant.holding"""
@@ -21,6 +20,7 @@ class Ant(SolidEntity):
         """Empty the Ant.holding if it isn't None """
         if (self.holding is None)== False:
             self.holding = None
+
 
 
 
