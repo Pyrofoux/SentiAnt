2529b1f6fdc7c22039404299fa2e2f0ac61eb192
Hugo <hfournier001@ensc.fr>
2018-12-04 18:18:06 +0100

    MTN c'est CFG

--- Sentiant/Model/Pheromone.py
+++ Sentiant/Model/Pheromone.py
@@ -1,9 +1,9 @@
-from Sentiant.Model import _
+from Sentiant.Model import Cfg
 
 class Pheromone:
 
     def __init__(self, id, baseLocation):
-        self.hpRadius = _.HPRADIUS
+        self.hpRadius = Cfg.HPRADIUS
         self.id = id
         (self.baseLocationX, self.baseLocationY) = baseLocation
 
