1a0fbd54da35966bcc653fc4391e3856379c5d49
GuillaumeCreusot <gcreusot@ensc.fr>
2018-12-10 17:54:06 +0100

    Layer UnitTest Added and debug Layer

--- Sentiant/Model/Ant.py
+++ Sentiant/Model/Ant.py
@@ -10,16 +10,18 @@ class Ant(SolidEntity):
         self._name = name # id of the ant, string of 5 chars
         self._team = team
         self._holding = None # by default, the ant doesn't carry anything
-        self._HP = Cfg.Cfg.HPMAX  # number of hits the ant can take before dying
-        self._FoV = Cfg.Cfg.FOV # Field Of View : number of cells the ant can view, center being itself
+        self._HP = Cfg.HPMAX  # number of hits the ant can take before dying
+        self._FoV = Cfg.FOV # Field Of View : number of cells the ant can view, center being itself
 
-        self._nextAction = Cfg.Cfg.SLEEP # next Action, will be read by Turn Manager
-        self._nextActionArg = Cfg.Cfg.NULL
+        self._nextAction = Cfg.SLEEP # next Action, will be read by Turn Manager
+        self._nextActionArg = Cfg.NULL
 
-
-
+    def __str__(self):
+        return "Name:{0}\nTeam:{1}\nHolding:{2}\nHp:{3}\nFov:{4}\nNextAction:{5}\nNextActionArg:{6}"\
+            .format(self._name, self._team, self._holding, self._HP, self._FoV, self._nextAction, self._nextActionArg)
     def move(self, direction):
-
+        """Set Action of ant to move
+        direction : 'up', 'left', ..."""
         direc = Cfg.parseDirection(direction)
 
         if(direc != Cfg.NULL):
@@ -73,4 +75,9 @@ class Ant(SolidEntity):
 if __name__ == '__main__':
     ant = Ant(0, "test", "test")
 
+    print(ant)
+
+    ant.move("up")
+
+    print(ant)
 
