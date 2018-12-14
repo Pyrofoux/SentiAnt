c875979a90c57402827ef6c8fd3e61e02eac718b
GuillaumeCreusot <gcreusot@ensc.fr>
2018-12-14 17:41:31 +0100

    Ant army

--- Sentiant/Main.py
+++ Sentiant/Main.py
@@ -7,7 +7,7 @@ import os
 
 if __name__ == '__main__':
 
-    #os.chdir("..\\")
+    os.chdir("..\\")
 
     mapGen = MapManager(width=10, height=10)
     mapGen.RegisterQueen(QueenTile(1, "team1"), Point(1, 1))
@@ -17,6 +17,12 @@ if __name__ == '__main__':
     #Ajout de la fourmi d'essai
     roger = FourmiRouge(0,"ROGER","red")
     map.layerSolid.Append(roger,Point(3,3))
+    roger1 = FourmiRouge(1, "ROGER", "red")
+    map.layerSolid.Append(roger1, Point(4, 3))
+    roger2 = FourmiRouge(2, "ROGER", "red")
+    map.layerSolid.Append(roger2, Point(3, 1))
+    roger3 = FourmiRouge(3, "ROGER", "red")
+    map.layerSolid.Append(roger3, Point(4, 1))
     turnmanager = TurnManager(map)
 
     view = MainView(map, turnmanager, (500, 500))
--- Sentiant/bot/FourmiRouge/FourmiRouge.py
+++ Sentiant/bot/FourmiRouge/FourmiRouge.py
@@ -7,5 +7,5 @@ class FourmiRouge(Ant):
         super().__init__(id, name, team)
 
     def newTurn(self):
-        #self.Move(Cfg.DOWN)
-        self.Phero(2)
+        self.Move(Cfg.DOWN)
+        #self.Phero(2)
