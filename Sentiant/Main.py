c4326dc04734dedddfd976b7131dda76be5bb436
GuillaumeCreusot <gcreusot@ensc.fr>
2018-12-14 15:40:32 +0100

    Fix spawn of nest

--- Sentiant/Main.py
+++ Sentiant/Main.py
@@ -9,7 +9,7 @@ if __name__ == '__main__':
     os.chdir("..\\")
 
     mapGen = MapManager(width=10, height=10)
-    mapGen.RegisterQueen(QueenTile(1, "team"), Point(4, 4))
+    mapGen.RegisterQueen(QueenTile(1, "team"), Point(1, 1))
 
     map = mapGen.Generate()
 
