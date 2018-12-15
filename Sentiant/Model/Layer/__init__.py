136f1a4801f1478a634b6c3a6f914f4c8807ff98
Louis Hache <36883441+LouisHache@users.noreply.github.com>
2018-12-15 17:56:05 +0100

    Fixs, mais nouveaux bugs

--- Sentiant/Model/Layer/__init__.py
+++
@@ -1,7 +0,0 @@
-"""Ce package contiendra les classes Layer"""
-
-# Avoid loading everything for simple units tests.
-from Sentiant.Model.Layer.Layer import  Layer
-from Sentiant.Model.Layer.LayerSolid import LayerSolid
-from Sentiant.Model.Layer.LayerPheromone import LayerPheromone
-from Sentiant.Model.Layer.LayerFloor import LayerFloor
