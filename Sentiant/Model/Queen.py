d6de57a57aa4d1fb35742f63b15a83964a188360
GuillaumeCreusot <gcreusot@ensc.fr>
2018-12-15 14:37:20 +0100

    Ant army

---
+++ Sentiant/Model/Queen.py
@@ -0,0 +1,8 @@
+class Queen:
+    def __init__(self, team):
+
+        #property
+        self._team = team
+
+    def SpawnAnt(self, name, type):
+
---
+++ Sentiant/Model/QueensManager.py
@@ -0,0 +1,19 @@
+from .Queen import Queen
+
+class QueensManager:
+
+    def __init__(self, nbQueen, teamsName, mapManager):
+        self.queens = {}
+        for i in range(nbQueen):
+            self.queens[i] =
+
+
+    def Spawn(self):
+
+
+    def GetDistribution(self):
+
+
+
+
+
