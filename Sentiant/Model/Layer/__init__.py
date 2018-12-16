2846fe9a44d6d19e06e9498ca81fefa867b77388
Louis Hache <36883441+LouisHache@users.noreply.github.com>
2018-12-16 11:44:47 +0100

    Un jour, Git fonctionnera, mais ce jour n'est pas encore arrivé

---
+++ Sentiant/Model/Layer/__init__.py
@@ -0,0 +1,7 @@
+"""Ce package contiendra les classes Layer"""
+
+# Avoid loading everything for simple units tests.
+from Sentiant.Model.Layer.Layer import  Layer
+from Sentiant.Model.Layer.LayerSolid import LayerSolid
+from Sentiant.Model.Layer.LayerPheromone import LayerPheromone
+from Sentiant.Model.Layer.LayerFloor import LayerFloor
